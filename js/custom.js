cf0036ec6f55c5fc5f63f5c49a80cf35d08b91d0
Simon Wex <simon@simonwex.com>
2014-03-14 01:25:23 -0700

    Only showing RSVP for valid invites

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
 
+	var urlParams;
+	(window.onpopstate = function () {
+	    var match,
+	        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+	        search = /([^&=]+)=?([^&]*)/g,
+	        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+	        query  = window.location.search.substring(1);
+
+	    urlParams = {};
+	    while (match = search.exec(query))
+	       urlParams[decode(match[1])] = decode(match[2]);
+	})();
+
+	function getUrlParams(){
+		return urlParams;
+	}
+
+	console.log(getUrlParams());
+
+	if (!urlParams.id){
+		$(".rsvp").hide();
+	}
+
+
 	/*-----------------------------------------------------------------------------------*/
 	/*	Smooth Scroll
 	/*  Thanks to: https://github.com/davist11/jQuery-One-Page-Nav
@@ -64,20 +88,20 @@ $(document).ready(function() {
 
 		// this is the id of the form
 		var formID = $("#js-form");
-		
+
 		// submits form with ajax method
 		formID.on("submit", function() {
 
 			$.ajax({
 				url: "mailer.php",
-				type: "POST",		    	
+				type: "POST",
 		        data: formID.serialize(), // serializes the form's elements.
 
 		        success: function(data) {
 		        	$(".js-display")
 		        				.addClass("message-panel")
 		        				.html(data); // show response from the php script.
-		        }		    
+		        }
 
 		    });
 
@@ -88,10 +112,10 @@ $(document).ready(function() {
 		// Show/Hide RSVP Menu selection on accept/decline
 		$(".decline").on("click", function(){
 			$(".rsvp-meal-choice").fadeOut();
-		});	
+		});
 		$(".accept").on("click", function(){
 			$(".rsvp-meal-choice").fadeIn();
-		});	
+		});
 
 	}
 	rsvpFormSubmit();
@@ -100,7 +124,7 @@ $(document).ready(function() {
 });
 
 /*-----------------------------------------------------------------------------------*/
-/*	Google Map API 
+/*	Google Map API
 /*  Credit to: http://stiern.com/tutorials/adding-custom-google-maps-to-your-website/
 /*-----------------------------------------------------------------------------------*/
 
@@ -116,7 +140,7 @@ function initialize() {
 	/* For details on styling go to the link below:
 	/* http://www.evoluted.net/thinktank/web-design/custom-google-maps-style-tool */
 	/*----------------------------------------------------------------------------*/
-	
+
 	var featureOpts = [
 		{
 			"featureType": "road",
